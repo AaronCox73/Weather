1b1f7cd0e7d14cc89e186f482ddb7a2fee08e10f
Aaron Cox <aaroncox@Aarons-iMac.local>
2021-09-05 08:36:28 -0600

    have search functions clean just need to css style and clear 5 day forecast before setting new forecast

--- assets/script.js
+++ assets/script.js
@@ -10,38 +10,6 @@ var input = document.getElementById("searchBox")
 var button = document.querySelector(".searchBtn")
 var cityValue = ""
 
-
-
-// function for weather in Kiel, right when page loads
-function stockWeather() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Kiel&units=imperial&appid=9504c29e0d15333519b3aa6f3c2b9734")
-        .then(function (response) {
-            response.json().then(function (data) {
-                console.log(data.main.temp, data.wind, data.main.humidity, data.name,);
-
-
-                var cityNameEl = document.querySelector("#cityName");
-                cityNameEl.append(data.name)
-
-                var tempEl = document.querySelector("#temperature");
-                tempEl.append(data.main.temp + "°f");
-
-                var windEl = document.querySelector("#wind");
-                windEl.append(data.wind.speed + "mph");
-
-                var humidityEl = document.querySelector("#humid");
-                humidityEl.append(data.main.humidity);
-
-                //var skiesEl = document.querySelector("#skies");
-                //skiesEl.append(data.weather);
-            })
-
-
-        })
-}
-//loads stockWeather function when page is first opened
-window.onload = stockWeather();
-
 //allows user to type in any city and get the weather for that city
 function searchWeather() {
     var url = fetch(api + input.value + units + apiKey)
@@ -59,10 +27,13 @@ function searchWeather() {
 
                 var humidityEl = document.querySelector("#humid");
                 $(humidityEl).empty().append("Humidity: " + data.main.humidity);
+
+                forecastSrch()
             })
         })
 }
 
+// Runs function for buttons 
 function cityButtons() {
     //when city button is clicked the corresponding weather is loaded 
     var url = fetch(api + cityValue + units + apiKey)
@@ -86,6 +57,7 @@ function cityButtons() {
         })
 }
 
+// running forecast 5-day for the buttons 
 function forecast() {
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + cityValue + "&appid=9504c29e0d15333519b3aa6f3c2b9734&units=imperial")
         .then(function (response) {
@@ -102,7 +74,42 @@ function forecast() {
                 forecastEl.innerHTML = forecastEl.innerHTML + `
                 <div class="col">
 
-                <div class="card" style="width: 12rem;">
+                <div class="card" style="width: 10rem;">
+
+                    <div class="card-body">
+                        <h5 class="card-title">${moment(fiveDayCastArray[index].dt, "X").format("MM/DD/YYYY")}</h5>
+                        <img src=" http://openweathermap.org/img/wn/${fiveDayCastArray[index].weather[0].icon}.png">
+                        <p class="card-text">Temp: ${fiveDayCastArray[index].main.temp}</p>
+                        <p>Wind: ${fiveDayCastArray[index].wind.speed} mph</p>
+                        <p>Humidity: ${fiveDayCastArray[index].main.humidity}</p>
+
+                    </div>
+                </div>
+            </div>
+                `
+
+            }
+        })
+}
+
+//Function to run 5 day forecast for the search bar 
+function forecastSrch() {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + input.value + "&appid=9504c29e0d15333519b3aa6f3c2b9734&units=imperial")
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (fiveDayData) {
+            console.log(fiveDayData)
+            var fiveDayCastArray = fiveDayData.list
+            var forecastEl = document.querySelector(".forecast")
+            for (let index = 3; index < fiveDayCastArray.length; index = index + 8) {
+
+                console.log(fiveDayCastArray[index])
+
+                forecastEl.innerHTML = forecastEl.innerHTML + `
+                <div class="col-sm-2">
+
+                <div class="card" style="width: 10rem;">
 
                     <div class="card-body">
                         <h5 class="card-title">${moment(fiveDayCastArray[index].dt, "X").format("MM/DD/YYYY")}</h5>
